ec655f0ec495f0a6832b258a2bb67f082e8ad3a4
aubford <musicaubrey@gmail.com>
2016-08-16 14:32:34 -0600

    npm i worked

--- index.web.js
+++ index.web.js
@@ -1,1 +1,14 @@
 import React from 'react'
+import 
+import { Root } from './app/MobileRoot'
+
+
+const RachioRN = React.createClass({
+  render() {
+    return (
+      <Root />
+    )
+  }
+})
+
+AppRegistry.registerComponent('rachioRN', () => RachioRN)
